2d96a715e4a3de93ea5d960f0626c8713f066970
blackbeltcam <league@iMac.attlocal.net>
2018-07-23 13:31:02 -0700

    Started Unity Project

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject prefab = (GameObject)Instantiate(Resources.Load("FloorBlock"), new Vector3(i, 0, 0), Quaternion.identity);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
